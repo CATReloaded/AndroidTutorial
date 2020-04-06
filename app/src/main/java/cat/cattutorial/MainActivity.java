b80ee67815f6e47d81af7e7baab24fb9f2ad2f3e
Mohamed Hamdy <mohamed.hamdyeg98@gmail.com>
2020-04-06 23:31:41 +0200

    finish list adapter

--- app/src/main/java/cat/cattutorial/MainActivity.java
+++ app/src/main/java/cat/cattutorial/MainActivity.java
@@ -1,14 +1,43 @@
 package cat.cattutorial;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ContactAdapter adapter = new ContactAdapter();
+    ArrayList<Contact> contacts = new ArrayList<>();
+    FloatingActionButton fab;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv);
+        recyclerView.setAdapter(adapter);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contacts.add(new Contact("Mohamed" + new Random().nextFloat(), ""));
+                ArrayList<Contact> tempList = new ArrayList<>(contacts);
+                adapter.submitList(tempList);
+
+            }
+        });
+
     }
+
 }
